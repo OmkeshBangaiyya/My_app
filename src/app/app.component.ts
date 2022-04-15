e52a08b19093991b519afa45bb8bdfe16a77c417
omkesh <omkeshbangaiyya18@gmail.com>
2022-04-15 11:36:08 -0700

    databinding assignment commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-application';
+  title = 'My-app';
+  image = ''
+  userinput =''
+  useroutput =''
+  showoutput=()=>{
+    this.useroutput = this.userinput
+  }
+  onclick=()=>{
+    this.image='https://i.pinimg.com/474x/01/88/dc/0188dc41881e0e410b5375cdead5f49a.jpg'
+ }
 }
